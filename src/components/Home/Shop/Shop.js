59e78a88bcd1977226196cac4cb81dab42675e7d
adamzermane <adamzermane92@yahoo.com>
2022-07-07 16:44:02 +0700

    updated the shop

--- src/components/Home/Shop/Shop.js
+++ src/components/Home/Shop/Shop.js
@@ -1,9 +1,9 @@
-import { faPlaneDeparture } from '@fortawesome/free-solid-svg-icons';
+
 import React from 'react';
 import { useSelector } from 'react-redux'
 import ShopDisplay from '.'
 
-function shop(){
+function Shop(){
     const products = useSelector(state => state.cart.products)
     let plants = products.map(plant => 
         <ShopDisplay 
@@ -11,5 +11,22 @@ function shop(){
         name={plant.name}
         light={plant.price}
         purify={plant.purify}
+        beginner = {plant.beginner}
+        petFriendly = {plant.petFriendly}
+        img={plant.img}
+        />
+        )
+
+        return(
+            <div className='h-screen relative'>
+                <Header />
+                <ShopHero /> 
+                <div className='grid grid-cols-2 md:grid-cols-3 mx-8 mb-14 gap-x-8 gap-y-12'>
+                     {plants} 
+               </div>
+               <Footer />
+            </div>
         )
 }
+
+export default Shop; 
