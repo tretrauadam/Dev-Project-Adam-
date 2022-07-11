33fd766bc8adcb5cc3c3305f25d3077ce3126e9e
adamzermane <adamzermane92@yahoo.com>
2022-07-11 14:08:13 +0700

    shopCollection of products

--- src/components/Shop/Collection.js
+++ src/components/Shop/Collection.js
@@ -2,8 +2,10 @@ import React from 'react';
 import {useParams} from 'react-router';
 import {useSelector} from 'react-redux'; 
 import ShopDisplay from './ShopDisplay'
+import ShopHero from './ShopHero';
 import Nav from '../Header/Nav';
 import Footer from '../Footer/Footer';
+
 
 
 function Collections(){
@@ -28,7 +30,32 @@ function Collections(){
             break 
     }
 
-    
+    let plants = plantCollection.map(plant => 
+        <ShopDisplay
+        key={plant.id}
+        name={plant.name}
+        light={plant.light}
+        price={plant.price}
+        purify={plant.purify}
+        beginner={plant.purify}
+        petFriendly = {plant.petFriendly}
+        img={plant.img}
+
+         />)
+
+    return(
+        <div className='h-screen relative'>
+            <Nav /> 
+            <ShopHero /> 
+            <ShopNav /> 
+          <div className=' grid grid-cols-2 md:grid-cols-3 mx-8 mb-14 gap-x-8 gap-y-12'>
+            {plants}
+          </div>
+          <Footer/> 
+        </div>
+    )
+
+
 }
 
 export default Collections; 
