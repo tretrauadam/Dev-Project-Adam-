f8e60a3daf9c4fb06588aa6c54a15bf1057721c5
adamzermane <adamzermane92@yahoo.com>
2022-07-13 15:57:53 +0700

    reconfigured the tailwind for the text card

--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -7,19 +7,25 @@ const Nav = () => {
     const dispatch = useDispatch()
 
     return(
+       
         <nav className='flex h-18 py-4 w-full m-auto text-white'>
             <Link to='/'>
                 <h1 className='transform duration-300 hover:-translate-y-1 font-display font-thin tracking-wide m-auto pl-4 md:pl-12 text-2xl md:text-5xl'>Greenify</h1>
             </Link>
+            <label for="checked-toggle" class="inline-flex relative items-center cursor-pointer">
+                 <input type="checkbox" value="" id="checked-toggle" class="sr-only peer" checked />
+                 <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                 <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Checked toggle</span>
+           </label>
 
             <ul className='flex justify-end items-end pr-4 md:pr-24 w-full space-x-6 md:space-x-12 font-display font-thin md:font-light'>
                 <Link to='/about'>
-                    <li className='text-sm md:text-lg transition duration-300 transform hover:translate-y-1 hover:text-blue-500 border-blue-500 hover:border-b-2'>
+                    <li className='text-sm md:text-lg transition duration-300 transform hover:translate-y-1 hover:text-green-500 border-green-500 hover:border-b-2'>
                         About
                     </li>
                 </Link>
                 <Link to='/shop'>
-                    <li className='text-sm md:text-lg transition duraction-300 transform hover:translate-y-1 hover:text-blue-500 border-blue-500 hover:border-b-2'>
+                    <li className='text-sm md:text-lg transition duraction-300 transform hover:translate-y-1 hover:text-green-500 border-green-500 hover:border-b-2'>
                         Shop
                     </li>
                 </Link>
--- src/components/Home/TextCard.js
+++ src/components/Home/TextCard.js
@@ -9,19 +9,21 @@ function TextCards() {
     return(
         <div className='my-24 py-24 bg-gray-200 bg-opacity-40 w-full'>
             <div className='2xl:w-3/4 m-auto flex flex-col justify-center space-y-16'>
+                <div className='flex flex-col md:flex-row w-full m-auto justify-between space-y-6 md:space-y-0'>
                 <div className='flex justify-start md:w-1/2 mx-3 md:mx-0'>
                     <img src= {window} alt=''/>
                 </div>
+
                 <div className='flex flex-col md:w-5/12 space-y-8 justify-center text-center md:text-left mx-4 md:mr-6 2xl:mr-0'>
                     <h2 className='font-display text-2xl md:text-4xl'>Lets do this</h2>
-                    <p className='font-body text-lg md:text-l tracking-wide break-words leading-loose'>
+                    <p className='font-body text-lg md:text-xl tracking-wide break-words leading-loose'>
                         We established ourselves in the city of Hanoi, famed for its decorative and vibrant city, our first apartment in the city had a lovely balcony 
                         overlooking the lake. fortune favoured us for, we lived directly above the flower street market. some people buy clothes or a watch to treat themselves, 
-                        for us, we were always excited to add plants to our pretty balcony. 
-                   
+                        for us, we were always excited to add plants to our pretty balcony.   
                     </p>
                     <Link to="/shop">
-                    <button className='bg-red-500 font-body rounded-md text-lg sm:text-2xl p-2 sm:p-4 w-32 sm:w-48 transform duration-200 hover:-translate-y-1 hover:bg-red-500'>
+                    <button className='p-4 w-36 md:w-48 bg-red-500 font-body rounded-md text-white text-sm md:text-xl transform duration-200 
+                    hover:-translate-y-1 hover:bg-red-700'>
                             Shop Now
                         </button>
                     </Link>
@@ -29,23 +31,23 @@ function TextCards() {
             </div>
             <div className='flex flex-col-reverse md:flex-row w-full m-auto justify-between space-y-8 md:space-y-0'>
                 <div className='flex flex-col md:w-5/12 space-y-8 justify-center text-center md:text-left mx-4 md:mr-6 2xl:mr-0'>
-                    <h2 className='font-display pt-4 md:pt-0 text-2xl md:text-4xl'>Lets do this again</h2>
+                    <h2 className='font-display pt-4 md:pt-0 text-2xl md:text-4xl'>Explore Together.</h2>
                     <p className='font-body text-lg md:text-xl tracking-wide break-words leading-loose'>
                         Enlighten your home with a bit of sunshine, we can recommend the best plants for your living style, be it under a bridge or a skyscrapper apartment.
                     </p>
-                    <Link to='/shop'>
-                        <button className='bg-red-500 font-body rounded-md text-lg sm:text-2xl p-2 sm:p-4 w-32 sm:w-48 transform duration-200 hover:-translate-y-1 hover:bg-red-500'>
-                            let's order
+                    <Link to='/shop/airPurifying'>
+                        <button className='p-4 w-36 md:w-48 bg-red-500 font-body rounded-md text-white text-sm md:text-xl transform duration-200 
+                    hover:-translate-y-1 hover:bg-red-700'>
+                            Discover
                         </button>
                     </Link>
+                </div>
                     <div className='flex justify-start md:w-1/2 mx-3 md:mx-0'>
                         <img src={work} alt=''/>
                     </div>
-
-                </div>
+               </div>
             </div>
-
-        </div>
+         </div>
     )
 }
 
