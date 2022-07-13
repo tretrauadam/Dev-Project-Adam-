b2e042486508c1229f99e8556f38cd054e0044e5
adamzermane <adamzermane92@yahoo.com>
2022-07-13 11:56:17 +0700

    updated the router to include elements and not components

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
-import React from 'react';
-import './index.css';
-import store from './store';
-import { BrowserRouter as Switch, Router, Route, Routes} from 'react-router-dom';
-import { Provider } from 'react-redux';
-import Home from './components/Home/Home';
-import Cart from './components/Cart/Cart';
-import About from './components/About/About';
-import Shop from './components/Shop/Shop';
-import Collections from './components/Shop/Collection';
+import './index.css'
+import ScrollToTop from "./ScrollToTop";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import store from './store'
+import Home from './components/Home/Home'
+import Cart from './components/Cart/Cart'
+import About from './components/About/About'
+import Shop from './components/Shop/Shop'
+import Collection from './components/Shop/Collections'
 import ItemDetail from './components/Shop/ItemDetail';
-import ScrollToTop from './ScrollToTop';
-
 
 const App = () => {
-  return(
-    <Provider store={store}>
+    return (
       <Router>
-        <ScrollToTop/>
-        <Cart/>
-        <Switch>   
-          <Routes>
-          <Route exact path to='/'component={<Home/>}/>
-          <Route exact path to='/about' component={<About/>}/>
-          <Route exact path to='/shop' component={<Shop/>}/>
-          <Route exact path to='/shop/:collection' component={<Collections/>} />
-          <Route exact path to='/shop/collection/:name' component={<ItemDetail/>}/>
-         </Routes> 
-        </Switch>
-      </Router>
-    </Provider>
-  );
-};
+      <Provider store={store}>
+              
+                  <ScrollToTop />
+                  <Cart />    
+                  <Routes>
+                      <Route exact path='/' element={<Home/>}/>
+                      <Route exact path='/about' element={<About/>} />
+                      <Route exact path='/shop' element={<Shop/>} />
+                      <Route exact path='/shop/:collection' element={<Collection/>} />
+                      <Route exact path='/shop/collection/:name' element={<ItemDetail/>} />
+                    
+                    </Routes>             
+      </Provider>
+  </Router>
+    
+  )
+      
+      
+}
 
-export default App; 
+export default App;
--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -13,11 +13,11 @@ function Hero() {
             style={{backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.3)), url(${hero})`}}>
                 <div className='flex flex-col h-full px-4 md:m1-auto md:mt-auto w-full md:w-1/2 justify-center text-white font-display space-y-4 md:space-y-6'>
                     <p className='text-3xl md:text-5xl'>
-                    Delicious Buds  <br></br> Right to your Doorstep
+                    Organic plants <br></br> Right to your Doorstep
                     </p>
-                    <p className='font-light text-lg sm:text-2xl'> Shopping for Buds has never been easier </p>
+                    <p className='font-light text-lg sm:text-2xl p-4' > buy now  </p>
                     <Link to="/shop">
-                        <button className='bg-red-300 font-body rounded-md text-lg sm:text-2xl p-2 sm:p-4 w-32 sm:w-48 transform duration-200 hover:-translate-y-1 hover:bg-red-500'>
+                        <button className='bg-red-500 font-body rounded-md text-lg sm:text-2xl p-2 sm:p-4 w-32 sm:w-48 transform duration-200 hover:-translate-y-1 hover:bg-red-500'>
                             Shop Now
                         </button>
                     </Link>
